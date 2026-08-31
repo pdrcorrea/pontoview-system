8895798ffda584237f30f6d074788ec3b4c12d85
pdrcorrea <pedrhc258@gmail.com>
2026-08-31 19:56:47 -0300

    Load TV readability overrides

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./player/rotationController";
+import "./player-readability.css";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
