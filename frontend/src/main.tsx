6a7b5749d56723538427d0653e77e34e3cfcc154
pdrcorrea <pedrhc258@gmail.com>
2026-09-07 12:05:10 -0300

    fix: initialize portrait orientation controller

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./player/rotationController";
+import "./player/orientationController";
 import "./player-readability.css";
 import "./message-experience.css";
 
