14ca1e85cc95668ce71cd0aa9ac19991a94fdf3c
pdrcorrea <pedrhc258@gmail.com>
2026-09-11 10:49:44 -0300

    build(android-player): add build.gradle.kts

---
+++ android-player/app/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "br.com.pontoview.player"
+    compileSdk = 37
+
+    defaultConfig {
+        applicationId = "br.com.pontoview.player"
+        minSdk = 23
+        targetSdk = 37
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
